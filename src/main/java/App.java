2cc1fa870e42e9fb8e8acfd5c7faa47ef5b021e0
anmizantar <anmizantar@gmail.com>
2025-03-10 09:36:32 +0300

    Initial commit: добавлен проект

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,26 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getSound());
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getSound());
+
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean(HelloWorld.class);
+        System.out.println(helloWorld.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean(Cat.class);
+        System.out.println(cat.getSound());
+
+        boolean areSameReference = (bean == helloWorld);
+        {System.out.println(areSameReference);}
+
+        boolean areSameReference2 = (cat1 == cat2);
+        {
+            System.out.println(areSameReference2);
+        }
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,14 +1,24 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
-}
+
+    @Scope("prototype") @Bean(name = "cat")
+        public Cat getCat() {
+            Cat cat = new Cat();
+            cat.setSound("Mao!");
+            return cat;
+
+        }
+    }
+
